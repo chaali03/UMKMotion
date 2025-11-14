ca34b5b25e982753bd229e6b992891b79980bca2
Satriyo Agung <sazhumaakun@gmail.com>
2025-11-14 07:23:31 +0700

    checkout

--- src/LandingPage/components/header/header.tsx
+++ src/LandingPage/components/header/header.tsx
@@ -86,6 +86,12 @@ export default function HomeHeader({ localTheme, setLocalTheme }: HomeHeaderProp
       label: "Konsultan", 
       icon: Lightbulb,
       description: "Konsultasi bisnis UMKM"
+    },
+    { 
+      href: "/checkout", 
+      label: "Checkout", 
+      icon: ShoppingCart,
+      description: "Selesaikan pembelian Anda"
     }
   ];
 
